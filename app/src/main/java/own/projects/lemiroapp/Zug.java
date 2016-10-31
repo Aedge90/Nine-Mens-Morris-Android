1599a2c46a8d7b1badebe0bd3d76a21490896be3
Achim <achim.ad.daeubler@fau.de>
2016-10-31 15:33:48 +0100

    added autogenerated equals and hash to Zug

--- app/src/main/java/own/projects/lemiroapp/Zug.java
+++ app/src/main/java/own/projects/lemiroapp/Zug.java
@@ -75,5 +75,27 @@ public class Zug {
 			throw new IllegalStateException("Illegal State in Zug!" + this);
 		}
 	}
-	
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Zug zug = (Zug) o;
+
+        if (set != null ? !set.equals(zug.set) : zug.set != null) return false;
+        if (src != null ? !src.equals(zug.src) : zug.src != null) return false;
+        if (dest != null ? !dest.equals(zug.dest) : zug.dest != null) return false;
+        return kill != null ? kill.equals(zug.kill) : zug.kill == null;
+
+    }
+
+    @Override
+    public int hashCode() {
+        int result = set != null ? set.hashCode() : 0;
+        result = 31 * result + (src != null ? src.hashCode() : 0);
+        result = 31 * result + (dest != null ? dest.hashCode() : 0);
+        result = 31 * result + (kill != null ? kill.hashCode() : 0);
+        return result;
+    }
 }
