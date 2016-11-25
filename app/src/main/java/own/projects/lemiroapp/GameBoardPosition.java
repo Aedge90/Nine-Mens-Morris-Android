6a0c083e99afbaa4fd41550ca160cf213573e56b
Achim <achim.ad.daeubler@fau.de>
2016-11-25 15:29:43 +0100

    GameBoardPosition now has a connect function

--- app/src/main/java/own/projects/lemiroapp/GameBoardPosition.java
+++ app/src/main/java/own/projects/lemiroapp/GameBoardPosition.java
@@ -7,6 +7,7 @@ public class GameBoardPosition extends Position {
     private GameBoardPosition right;
     private GameBoardPosition up;
     private GameBoardPosition down;
+    private Options.Color color;
 
     GameBoardPosition(int x, int y) {
         super(x, y);
@@ -16,19 +17,46 @@ public class GameBoardPosition extends Position {
         super(pos);
     }
 
-    public void setLeft(GameBoardPosition left) {
-        this.left = left;
+    public void setColor(Options.Color color) {
+        this.color = color;
     }
 
-    public void setRight(GameBoardPosition right) {
+    public void connectRight(GameBoardPosition right) {
         this.right = right;
+        right.left = this;
     }
 
-    public void setUp(GameBoardPosition up) {
-        this.up = up;
-    }
-
-    public void setDown(GameBoardPosition down) {
+    public void connectDown(GameBoardPosition down) {
         this.down = down;
+        down.up = this;
     }
+
+    public Options.Color getColor() {
+        return this.color;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        if (!super.equals(o)) return false;
+
+        GameBoardPosition that = (GameBoardPosition) o;
+
+        if (left != null ? !left.equals(that.left) : that.left != null) return false;
+        if (right != null ? !right.equals(that.right) : that.right != null) return false;
+        if (up != null ? !up.equals(that.up) : that.up != null) return false;
+        return down != null ? down.equals(that.down) : that.down == null;
+
+    }
+
+    @Override
+    public int hashCode() {
+        int result = left != null ? left.hashCode() : 0;
+        result = 31 * result + (right != null ? right.hashCode() : 0);
+        result = 31 * result + (up != null ? up.hashCode() : 0);
+        result = 31 * result + (down != null ? down.hashCode() : 0);
+        return result;
+    }
+
 }
