5146356cbf0406f4f9fe0e87fc7133ce10e07e3a
Achim <achim.ad.daeubler@gmail.com>
2017-01-10 22:53:29 +0100

    showing toasts now cancels running ones

--- app/src/main/java/own/projects/lemiroapp/GameModeActivity.java
+++ app/src/main/java/own/projects/lemiroapp/GameModeActivity.java
@@ -1,19 +1,3 @@
-/*
- * Copyright (C) 2012 The Android Open Source Project
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
 package own.projects.lemiroapp;
 
 import android.view.View;
@@ -60,6 +44,7 @@ public class GameModeActivity extends android.support.v4.app.FragmentActivity{
 	int screenWidth;
 	ImageView redSector;
 	ImageView[] millSectors;
+    Toast lastToast;
 	final GameModeActivity THIS = this;
 
     volatile State state;
@@ -413,12 +398,15 @@ public class GameModeActivity extends android.support.v4.app.FragmentActivity{
 
     }
 
-    //TODO hide previous toast when showing a new one so they wont stack
-
+    //shows a Toast and cancels others if they are showing
 	protected void showToast(String text){
+        if(lastToast != null) {
+            lastToast.cancel();
+        }
 		Toast toast = Toast.makeText(this,text ,Toast.LENGTH_SHORT);
 		toast.setGravity(Gravity.BOTTOM,0,0);
 		toast.show();
+        lastToast = toast;
 	}
 	
 	void setTextinUIThread(final TextView view, final String text){
