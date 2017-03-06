6d8e1c217e1b8c15b9322f38316acae51f8718df
Achim <achim.ad.daeubler@fau.de>
2017-03-06 20:08:18 +0100

    removed unused menu code

--- app/src/main/java/com/github/aedge90/nmm/MainActivity.java
+++ app/src/main/java/com/github/aedge90/nmm/MainActivity.java
@@ -43,13 +43,6 @@ public class MainActivity extends Activity {
     }
 
     @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.activity_main, menu);
-        return true;
-    }
-
-    @Override
     public void onActivityResult(int requestCode, int resultCode, Intent data) {
         if(requestCode == SET_OPTIONS) {
             if (resultCode == RESULT_OK) {
