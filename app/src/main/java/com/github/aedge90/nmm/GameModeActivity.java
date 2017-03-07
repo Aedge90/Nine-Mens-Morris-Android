f1f87641409e2aa58668ed62492b0fbfc25ac174
Achim <achim.ad.daeubler@fau.de>
2017-03-07 17:32:09 +0100

    changed strings to be different on human vs bot

--- app/src/main/java/com/github/aedge90/nmm/GameModeActivity.java
+++ app/src/main/java/com/github/aedge90/nmm/GameModeActivity.java
@@ -461,19 +461,28 @@ public class GameModeActivity extends android.support.v4.app.FragmentActivity{
         if(currPlayer.getDifficulty() == null) {
             //the player is a human
             message = getResources().getString(R.string.you_have_won);
+            if(field.getState(currPlayer).equals(GameBoard.GameState.WON_NO_MOVES)){
+                showGameOverMsg(message, getResources().getString(R.string.no_moves_human));
+                setTextinUIThread(progressText, message);
+                return true;
+            }else if (field.getState(currPlayer).equals(GameBoard.GameState.WON_KILLED_ALL)) {
+                showGameOverMsg(message, getResources().getString(R.string.no_pieces_human));
+                setTextinUIThread(progressText, message);
+                return true;
+            }
         }else{
             message = winningColor + " " + getResources().getString(R.string.has_won);
+            if(field.getState(currPlayer).equals(GameBoard.GameState.WON_NO_MOVES)){
+                showGameOverMsg(message, getResources().getString(R.string.no_moves_bot));
+                setTextinUIThread(progressText, message);
+                return true;
+            }else if (field.getState(currPlayer).equals(GameBoard.GameState.WON_KILLED_ALL)) {
+                showGameOverMsg(message, getResources().getString(R.string.no_pieces_bot));
+                setTextinUIThread(progressText, message);
+                return true;
+            }
         }
 
-        if(field.getState(currPlayer).equals(GameBoard.GameState.WON_NO_MOVES)){
-            showGameOverMsg(message, getResources().getString(R.string.no_moves));
-            setTextinUIThread(progressText, message);
-            return true;
-        }else if (field.getState(currPlayer).equals(GameBoard.GameState.WON_KILLED_ALL)) {
-            showGameOverMsg(message, getResources().getString(R.string.no_pieces));
-            setTextinUIThread(progressText, message);
-            return true;
-        }
         return false;
     }
     
