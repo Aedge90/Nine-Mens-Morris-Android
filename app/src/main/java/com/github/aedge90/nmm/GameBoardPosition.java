5e2e22ef387b63187f2d72f5e7c08b75157dd6f7
Achim <achim.ad.daeubler@fau.de>
2017-03-22 14:49:06 +0100

    removed equals method of GameBoardPosition
    - it was a source of error, as I often compare Position with GameBoardPosition
    - these two should have the same equals
    - also the equals method used itself, which is probably not good

--- app/src/main/java/com/github/aedge90/nmm/GameBoardPosition.java
+++ app/src/main/java/com/github/aedge90/nmm/GameBoardPosition.java
@@ -64,28 +64,4 @@ public class GameBoardPosition extends Position {
         return this.color;
     }
 
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        if (!super.equals(o)) return false;
-
-        GameBoardPosition that = (GameBoardPosition) o;
-
-        if (left != null ? !left.equals(that.left) : that.left != null) return false;
-        if (right != null ? !right.equals(that.right) : that.right != null) return false;
-        if (up != null ? !up.equals(that.up) : that.up != null) return false;
-        return down != null ? down.equals(that.down) : that.down == null;
-
-    }
-
-    @Override
-    public int hashCode() {
-        int result = left != null ? left.hashCode() : 0;
-        result = 31 * result + (right != null ? right.hashCode() : 0);
-        result = 31 * result + (up != null ? up.hashCode() : 0);
-        result = 31 * result + (down != null ? down.hashCode() : 0);
-        return result;
-    }
-
 }
