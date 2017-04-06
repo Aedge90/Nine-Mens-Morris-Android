86231533876fe5b60e4824fbb5d8b5e962a07618
Achim <achim.ad.daeubler@fau.de>
2017-04-06 09:32:46 +0200

    now managing bitmap manually, to have more control

---
+++ app/src/main/java/com/github/aedge90/nmm/BitmapManager.java
@@ -0,0 +1,45 @@
+package com.github.aedge90.nmm;
+
+
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+
+public class BitmapManager {
+
+    private GameModeActivity c;
+
+    private final Bitmap blackPieceBitmap;
+    private final Bitmap whitePieceBitmap;
+    private final Bitmap greenPieceBitmap;
+    private final Bitmap redPieceBitmap;
+
+    public BitmapManager(GameModeActivity c) {
+
+        this.c = c;
+
+        int size = c.screenWidth / GameBoard.LENGTH;
+        float scaleFactor = 1.16f;
+        blackPieceBitmap = createScaledBitmap(R.drawable.piece_black, (int) (size * scaleFactor));
+        whitePieceBitmap = createScaledBitmap(R.drawable.piece_white, (int) (size * scaleFactor));
+        greenPieceBitmap = createScaledBitmap(R.drawable.piece_green, (int) (size * scaleFactor));
+        redPieceBitmap = createScaledBitmap(R.drawable.piece_red, (int) (size * scaleFactor));
+    }
+
+    private Bitmap createScaledBitmap(int resId, int size){
+        return Bitmap.createScaledBitmap(BitmapFactory.decodeResource(c.getResources(), resId), size, size, true);
+    }
+
+    public Bitmap getBitmap(Options.Color color){
+        if (color.equals(Options.Color.BLACK)) {
+            return blackPieceBitmap;
+        } else if (color.equals(Options.Color.WHITE)) {
+            return whitePieceBitmap;
+        } else if (color.equals(Options.Color.RED)) {
+            return redPieceBitmap;
+        }else if (color.equals(Options.Color.GREEN)) {
+            return greenPieceBitmap;
+        }
+        return null;
+    }
+}
+
