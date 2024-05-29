f54b7ec167aa97a4ca29d8c89120b2113bdd1bb9
Achim <achim.ad.daeubler@gmail.com>
2024-05-29 20:47:41 +0200

    updated to proper target sdk versions

--- app/src/main/java/com/github/aedge90/nmm/Options.java
+++ app/src/main/java/com/github/aedge90/nmm/Options.java
@@ -5,38 +5,40 @@ import android.os.Parcelable;
 
 public class Options implements Parcelable {
 
-	protected MillVariant millVariant;
-	protected static enum MillVariant {
-		MILL5, MILL7, MILL9
-	};
+    protected MillVariant millVariant;
+
+    protected enum MillVariant {
+        MILL5, MILL7, MILL9
+    }
 
     protected Player playerWhite;
-	protected Player playerBlack;
+    protected Player playerBlack;
 
     //TODO add EASIEST with depth 0
-	protected enum Difficulties {
+    protected enum Difficulties {
         EASIER, EASY, NORMAL, ADVANCED, HARD, HARDER, HARDEST
-	}
+    }
 
-	protected Color whoStarts;
-	protected enum Color {
-		WHITE, BLACK, RED, GREEN, NOTHING, INVALID
-	}
-	
-	public Options() {
-		this.millVariant = MillVariant.MILL9;
-		this.whoStarts = Color.WHITE;
-		this.playerWhite = new Player(Options.Color.WHITE);
-		this.playerBlack = new Player(Options.Color.BLACK);
-	}
-	
-	public Options(Parcel in) {
-		Options o = in.readParcelable(getClass().getClassLoader());
-		this.millVariant = o.millVariant;
+    protected Color whoStarts;
+
+    protected enum Color {
+        WHITE, BLACK, RED, GREEN, NOTHING, INVALID
+    }
+
+    public Options() {
+        this.millVariant = MillVariant.MILL9;
+        this.whoStarts = Color.WHITE;
+        this.playerWhite = new Player(Options.Color.WHITE);
+        this.playerBlack = new Player(Options.Color.BLACK);
+    }
+
+    public Options(Parcel in) {
+        Options o = in.readParcelable(getClass().getClassLoader());
+        this.millVariant = o.millVariant;
         this.whoStarts = o.whoStarts;
         this.playerWhite = o.playerWhite;
-		this.playerBlack = o.playerBlack;
-	}
+        this.playerBlack = o.playerBlack;
+    }
 
     @Override
     public String toString() {
@@ -57,17 +59,17 @@ public class Options implements Parcelable {
 
     // write your object's data to the passed-in Parcel
     @Override
-	public void writeToParcel(Parcel dest, int flags) {
+    public void writeToParcel(Parcel dest, int flags) {
         dest.writeValue(millVariant);
         dest.writeValue(whoStarts);
         dest.writeValue(playerWhite);
         dest.writeValue(playerBlack);
-	}
+    }
 
     // this is used to regenerate your object. All Parcelables must have a CREATOR that implements these two methods
     public static final Parcelable.Creator<Options> CREATOR = new Parcelable.Creator<Options>() {
         public Options createFromParcel(Parcel source) {
-           final Options o = new Options();
+            final Options o = new Options();
             o.millVariant = (MillVariant) source.readValue(Options.class.getClassLoader());
             o.whoStarts = (Color) source.readValue(Options.class.getClassLoader());
             o.playerWhite = (Player) source.readValue(Options.class.getClassLoader());
@@ -76,8 +78,8 @@ public class Options implements Parcelable {
         }
 
         public Options[] newArray(int size) {
-           throw new UnsupportedOperationException();
+            throw new UnsupportedOperationException();
         }
     };
-    
+
 }
